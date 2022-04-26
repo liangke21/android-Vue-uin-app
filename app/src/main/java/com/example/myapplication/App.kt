868af3dd60365aaef7f9446feacf28ca2146b673
liangke <1396797522@qq.com>
2022-04-26 15:31:11 +0800

    项目搭建

---
+++ app/src/main/java/com/example/myapplication/App.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+import io.dcloud.application.DCloudApplication
+
+class App: DCloudApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
